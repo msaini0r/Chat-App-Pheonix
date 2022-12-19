23a14378090451e861dac5b7fe389ebdc6718683
msaini0r <mohit.12012540@lpu.in>
2022-12-20 01:14:38 +0530

    Register, Login & Chat component in progress.

---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+function Register() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("form");
+  };
+
+  const handleChange = (event) => {};
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src="" alt="" />
+            <h1>Pheonix</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+    </>
+  );
+}
+const FormContainer = styled.div``;
+export default Register;
